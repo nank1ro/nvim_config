e13dbb74bee9d8e0c0440cd8aa26cf4eb397cac1
Alexandru Mariuti <alex@mariuti.com>
2025-07-30 16:32:18 +0200

    add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "javascript",
+        "typescript",
+        "dart",
+        "json"
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+    })
+  end,
+}
